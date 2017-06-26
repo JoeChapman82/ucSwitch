a77512c382dd7b10dcc19481aeebbde395abb092
Joe Chapman <contemno82@gmail.com>
2017-06-26 16:23:43 +0100

    updated as requested 2

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Universal Credit Switch',
+  serviceName: 'Universal Credit',
 
   // Default port that prototype runs on
   port: '3000',
